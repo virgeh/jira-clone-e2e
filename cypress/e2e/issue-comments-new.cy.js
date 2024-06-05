0c71521072a3617a29c284b459af663f385e8c01
virgeh <virgehaavasalu@gmail.com>
2024-06-05 17:49:52 +0300

    My commets about the commit

--- cypress/e2e/issue-comments-new.cy.js
+++ cypress/e2e/issue-comments-new.cy.js
@@ -1,54 +1,50 @@
-describe("Issue comments creating, editing and deleting", () => {
+describe("Issue comments creating, editing, and deleting", () => {
   beforeEach(() => {
     cy.visit("/");
     cy.url()
-      .should("include", `${Cypress.env("baseUrl")}project/board`)
+      .should("include", `${Cypress.env("baseUrl")}project`)
       .then((url) => {
         cy.visit(url + "/board");
+        cy.url().should("include", "/board");
         cy.contains("This is an issue of type: Task.").click();
+        cy.get('[data-testid="modal:issue-details"]').should("be.visible");
       });
   });
 
   const getIssueDetailsModal = () =>
     cy.get('[data-testid="modal:issue-details"]');
 
-  it("Should create, edit, and delete a comment successfully", () => {
-    const comment = "TEST_COMMENT";
-    const editedComment = "TEST_COMMENT_EDITED";
+  function addComment(comment) {
+    cy.contains("Add a comment...").click();
+    cy.get('textarea[placeholder="Add a comment..."]').type(comment);
+    cy.contains("button", "Save").click().should("not.exist");
+  }
 
-    // Step 1: Add a comment
-    getIssueDetailsModal().within(() => {
-      cy.contains("Add a comment...").click();
+  function editComment(previousComment, newComment) {
+    cy.get('[data-testid="issue-comment"]')
+      .first()
+      .contains("Edit")
+      .click()
+      .should("not.exist");
+    cy.get('textarea[placeholder="Add a comment..."]')
+      .should("contain", previousComment)
+      .clear()
+      .type(newComment);
+    cy.contains("button", "Save").click().should("not.exist");
+  }
 
-      cy.get('textarea[placeholder="Add a comment..."]').type(comment);
+  function deleteComment() {
+    cy.get('[data-testid="issue-comment"]').contains("Delete").click();
+    cy.get('[data-testid="modal:confirm"]')
+      .contains("button", "Delete comment")
+      .click()
+      .should("not.exist");
+    getIssueDetailsModal()
+      .find('[data-testid="issue-comment"]')
+      .should("not.exist");
+  }
 
-      cy.contains("button", "Save").click().should("not.exist");
-
-      cy.contains("Add a comment...").should("exist");
-      cy.get('[data-testid="issue-comment"]').should("contain", comment);
-    });
-
-    // Step 2: Edit the added comment
-    getIssueDetailsModal().within(() => {
-      cy.get('[data-testid="issue-comment"]')
-        .first()
-        .contains("Edit")
-        .click()
-        .should("not.exist");
-
-      cy.get('textarea[placeholder="Add a comment..."]')
-        .clear()
-        .type(editedComment);
-
-      cy.contains("button", "Save").click().should("not.exist");
-
-      cy.get('[data-testid="issue-comment"]')
-        .should("contain", "Edit")
-        .and("contain", editedComment);
-    });
-
-    // Step 3: Remove the initial comment
-    //
+  it("Should delete a comment successfully", () => {
     getIssueDetailsModal()
       .find('[data-testid="issue-comment"]')
       .contains("Delete")
@@ -61,7 +57,37 @@ describe("Issue comments creating, editing and deleting", () => {
 
     getIssueDetailsModal()
       .find('[data-testid="issue-comment"]')
-      .click()
-      .should("be.visible");
+      .should("not.exist");
+  });
+
+  function assertCommentVisible(comment) {
+    cy.get('[data-testid="issue-comment"]').should("contain", comment);
+  }
+
+  function assertCommentNotVisible() {
+    cy.get('[data-testid="issue-comment"]').should("not.exist");
+  }
+
+  it("Should create, edit, and delete a comment successfully", () => {
+    const initialComment = "INITIAL_COMMENT";
+    const editedComment = "EDITED_COMMENT";
+
+    // Add a comment
+    getIssueDetailsModal().within(() => {
+      addComment(initialComment);
+      assertCommentVisible(initialComment);
+    });
+
+    // Edit the comment
+    getIssueDetailsModal().within(() => {
+      editComment(initialComment, editedComment);
+      assertCommentVisible(editedComment);
+    });
+
+    // Delete the comment
+    getIssueDetailsModal().within(() => {
+      deleteComment();
+      assertCommentNotVisible();
+    });
   });
 });
